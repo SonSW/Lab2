e805c02ac8c7626615a79033695952818c1109fb
Sewon Son <sewon1407@snu.ac.kr>
2026-04-08 13:19:13 +0900

    Edited main.cpp

--- main.cpp
+++ main.cpp
@@ -6,17 +6,21 @@ using namespace std;
 int N,M;
 vector<int> vec;
 
-void func(int cnt) {
-
-
-
-
-
-
-
+void func(int l) {
+    if(vec.size() >= M) {
+        for(int x : vec)
+            cout << x+1 << " ";
+        cout << endl;
+    } else {
+        for(int i=l; i<=N-M+vec.size(); i++) {
+            vec.push_back(i);
+            func(i+1);
+            vec.pop_back();
+        }
+    }
 }
 
 int main() {
-   cin >> N >> M;
-   func(0);
+    cin >> N >> M;
+    func(0);
 }
